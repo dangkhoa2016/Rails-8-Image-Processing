289c7d192c14204ab688d2f2ac6ba0cf639c3896
Dang Khoa <i.am@dangkhoa.dev>
2025-01-29 09:20:11 +0000

    add home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    render json: { message: "Welcome to Rails 8 API Authentication" }
+    render json: { message: "Welcome to Rails v8 API Image Processing with JWT Authentication" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :home, only: [ :index ]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :home, only: [ :index ]
 
